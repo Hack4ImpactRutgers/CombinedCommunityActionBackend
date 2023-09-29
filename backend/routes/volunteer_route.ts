36f900ca733858f51b2514373fbdfb7ac93a8f1f
jan <57322982+mvte@users.noreply.github.com>
2023-09-29 17:40:00 -0400

    add volunteer route

---
+++ backend/routes/volunteer_route.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+const Volunteer = require("../schemas/volunteer_schema");
+const router = express.Router();
+/*
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+*/
+
+router.get("/:id", (req: Request, res: Response) => {
+    Volunteer.findById(req.params.id).then(
+        (volunteer: any) => {
+            res.send(volunteer);
+        }
+    ).catch(
+        (err: any) => {
+            console.log(err);
+            res.send("volunteer " + req.params.id);
+        }
+    );
+});
+
+router.post("/", /*, [auth, admin], */ (req: Request, res: Response) => {
+    const newVolunteer = new Volunteer(req.body);
+    newVolunteer.save().then(
+        (volunteer: any) => {
+            res.send(volunteer);
+        }
+    ).catch(
+        (err: any) => {
+            res.send(err);
+        }
+    );
+});
+
+module.exports = router;
