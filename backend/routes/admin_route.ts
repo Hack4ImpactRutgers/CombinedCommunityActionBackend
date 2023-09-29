e152abf1123967abf5ce961dc60604d038ea0914
jan <57322982+mvte@users.noreply.github.com>
2023-09-29 17:39:22 -0400

    add admin route

---
+++ backend/routes/admin_route.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+const Admin = require("../schemas/admin_schema");
+const router = express.Router();
+/*
+const auth = require("../middleware/auth");
+const adminAuth = require("../middleware/admin");
+*/
+
+router.get("/:id", (req: Request, res: Response) => {
+    Admin.findById(req.params.id).then(
+        (admin: any) => {
+            res.send(admin);
+        }
+    ).catch(
+        (err: any) => {
+            console.log(err);
+            res.send("admin " + req.params.id);
+        }
+    )
+});
+
+router.post("/", /*, [auth, adminAuth], */ (req: Request, res: Response) => {
+    const newAdmin = new Admin(req.body);
+    newAdmin.save().then(
+        (admin: any) => {
+            res.send(admin);
+        }
+    ).catch(
+        (err: any) => {
+            res.send(err);
+        }
+    );
+});
+
+module.exports = router;
