de65424f4ea748179b6114296716f8b247d601f8
jan <57322982+mvte@users.noreply.github.com>
2023-11-17 19:27:42 -0500

    update admin schema to include email and password

--- backend/schemas/admin_schema.ts
+++ backend/schemas/admin_schema.ts
@@ -1,12 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const adminSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
   },
   {}
 );
 
-const adminModel = mongoose.model('admin', adminSchema);
+const adminModel = mongoose.model("admin", adminSchema);
 
 export default adminModel;
