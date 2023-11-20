935e7cdf30e40cb183defaecbd14dc236907bf48
jan <57322982+mvte@users.noreply.github.com>
2023-11-20 09:08:13 -0500

    update middleware tests to use volunteer route

--- backend/tests/middleware.test.ts
+++ backend/tests/middleware.test.ts
@@ -3,13 +3,14 @@ import { Server } from "http";
 import jwt, { Secret } from "jsonwebtoken";
 import { app } from "../index";
 import mongoose from "mongoose";
-import Admin from "../schemas/admin_schema";
+import Volunteer from "../schemas/volunteer_schema";
 
 describe("Middleware functions tests", () => {
   let server: Server;
   let adminToken: string;
   let volunteerToken: string;
   let invalidToken: string;
+  let volunteer: any;
 
   // Set up: start the server and create test tokens
   beforeAll(async () => {
@@ -33,13 +34,21 @@ describe("Middleware functions tests", () => {
       email: "testemail",
       roles: ["admin", "volunteer"]
     }, "incorrect secret");
+
+    // Create a test volunteer
+    volunteer = {
+      isActive: true,
+      name: "John Doe",
+      email: "testemail",
+      number: "1234567890",
+    };
   });
 
 
   // Clean up: Close the server, delete test entry, and disconnect from the database
   afterAll((done) => {
     server.close(() => {
-      Admin.deleteOne({ name: "John Doe" }).then(() => {
+      Volunteer.deleteOne({ name: "John Doe" }).then(() => {
         mongoose.disconnect().then(() => {
           done();
         });
@@ -49,24 +58,27 @@ describe("Middleware functions tests", () => {
 
   // ADMIN AUTHENTICATION TESTS
   describe("Admin authentication tests", () => {
-    it("Admins should be able to create new admins", async () => {
+    it("Admins should be able to create new volunteers", async () => {
       const response = await request(server)
-        .post("/admin")
+        .post("/volunteer")
         .set("x-auth-token", adminToken)
-        .send({ name: "John Doe" });
+        .send(volunteer);
 
       expect(response.status).toBe(201);
-      expect(response.body.name).toBe("John Doe");
+      expect(response.body.name).toBe(volunteer.name);
+      expect(response.body.email).toBe(volunteer.email);
+      expect(response.body.number).toBe(volunteer.number);
+      expect(response.body.isActive).toBe(volunteer.isActive);
     });
   });
 
   // VOLUNTEER AUTHENTICATION TESTS
   describe("Volunteer authentication tests", () => {
-    it("Volunteers should not be able to create new admins", async () => {
+    it("Volunteers should not be able to create new volunteers", async () => {
       const response = await request(server)
-        .post("/admin")
+        .post("/volunteer")
         .set("x-auth-token", volunteerToken)
-        .send({ name: "John Doe" });
+        .send(volunteer);
 
       expect(response.status).toBe(403);
       expect(response.body).toBe("Unauthorized");
@@ -75,11 +87,11 @@ describe("Middleware functions tests", () => {
 
   // INVALID TOKEN AUTHENTICATION TESTS
   describe("Invalid token authentication tests", () => {
-    it("Invalid tokens should not be able to create new admins", async () => {
+    it("Invalid tokens should not be able to create new volunteers", async () => {
       const response = await request(server)
-        .post("/admin")
+        .post("/volunteer")
         .set("x-auth-token", invalidToken)
-        .send({ name: "John Doe" });
+        .send(volunteer);
       
       expect(response.status).toBe(400);
       expect(response.body).toBe("Token is not valid");
@@ -88,10 +100,10 @@ describe("Middleware functions tests", () => {
 
   // NO TOKEN AUTHENTICATION TESTS
   describe("No token authentication tests", () => {
-    it("If a token is not provided, then we should not be able to create new admins", async () => {
+    it("If a token is not provided, then we should not be able to create new volunteers", async () => {
       const response = await request(server)
-        .post("/admin")
-        .send({ name: "John Doe" });
+        .post("/volunteer")
+        .send(volunteer);
       
       expect(response.status).toBe(401);
       expect(response.body).toBe("No token, authorization denied");
