346b4c05fbf469e7f84639c96d429d782589e3d2
jan <57322982+mvte@users.noreply.github.com>
2024-03-01 17:59:58 -0500

    prevent duplicate password changes requests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/public/"],
+  testPathIgnorePatterns: ["/node_modules/", "/public/", "/dist/"],
 };
