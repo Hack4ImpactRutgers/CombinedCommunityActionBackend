bcdc9b1b66bb1451c1ca8baca435c05184a04209
VanshSharma14 <goldenpotato14@gmail.com>
2024-03-26 20:48:15 -0400

    client test started

---
+++ backend/tests/client.test.ts
@@ -0,0 +1,23 @@
+import client from "../schemas/client_schema";
+import { Server } from "http";
+import request from "supertest";
+import Admin from "../schemas/admin_schema";
+import { app } from "../index";
+import mongoose from "mongoose";
+import jwt, { Secret } from "jsonwebtoken";
+
+// Describbe the test suite for client id routes
+describe("Client ID Routes", () => {
+  let server: Server;
+  let token: string;
+  let admin: any;
+  let client1: any;
+  let client2: any;
+  let client3: any;
+  let client4: any;
+
+  // Before all tests, create a new admin user and 4 client documents
+  beforeAll(async () => {
+    server = app.listen();
+    admin = new Admin({
+      email: "
